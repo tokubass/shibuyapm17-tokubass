4f062e711fe39df8848f1cd61f9981acea8c07c3
tokubass <tokubass@cpan.org>
2015-06-02 15:36:22 +0900

    update sql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,7 @@
+
 CREATE TABLE IF NOT EXISTS member (
     id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name         VARCHAR(255)
-);
+    name         VARCHAR(60) comment 'name comment',
+    code         INTEGER  comment 'code comment',
+  UNIQUE (`name`,`code`)
+) comment='会員情報';
